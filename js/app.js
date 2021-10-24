b9b894947272a4a91357e95671709f88e6e75520
vandanchauhan <vandan.s.chauhan@gmai.com>
2021-10-24 17:54:22 +0530

    color change

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#916C17"
+        "value": "#52878c"
       },
       "shape": {
         "type": "circle",
@@ -50,7 +50,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 200,
-        "color": "#916C17",
+        "color": "#52878c",
         "opacity": 0.4,
         "width": 1
       },
