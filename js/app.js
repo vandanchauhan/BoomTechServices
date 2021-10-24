e1b48b59bc528afe1bc1b27f141f9edafdbde570
vandanchauhan <vandan.s.chauhan@gmai.com>
2021-10-24 16:16:12 +0530

    Boomtech logo change

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 600,
+        "value": 750,
         "density": {
           "enable": true,
           "value_area": 1800
@@ -56,7 +56,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 2,
+        "speed": 3,
         "direction": "none",
         "random": false,
         "straight": false,
